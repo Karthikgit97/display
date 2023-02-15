43be2918ca69242c9a7c1e28b25beaea19acbcfe
Karthikgit97 <101712141+Karthikgit97@users.noreply.github.com>
2023-02-15 12:52:31 +0530

    Create soft_ser.cpp

---
+++ soft_ser.cpp
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Serial.println("Good");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(115200);
+}
+
+void loop() // run over and over
+{
+  if (mySerial.available())
+    Serial.write(mySerial.readString());
+  if (Serial.available())
+    mySerial.write(Serial.read());
+}
